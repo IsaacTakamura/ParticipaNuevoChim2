eb813dd2fc16d2c1553cb876ddfdfde90b52b20d
Isaac <isaactakamura107@gmail.com>
2025-02-26 16:49:14 -0500

    Refactoriza la función de envío de correos: mejora la legibilidad y mantiene la configuración del servidor SMTP.

--- forms/alumbrado/send_email.php
+++ forms/alumbrado/send_email.php
@@ -5,38 +5,33 @@ use PHPMailer\PHPMailer\Exception;
 require '../../vendor/autoload.php'; // Asegúrate de que la ruta sea correcta
 
 if (!function_exists('send_email')) {
-    // Esta función envía un correo electrónico de confirmación después de recibir un reporte
     function send_email($to, $subject, $message, $attachments = [])
     {
         $mail = new PHPMailer(true);
 
         try {
-            // Configuración del servidor SMTP
             $mail->isSMTP();
-            $mail->Host = 'mail.muninuevochimbote.gob.pe'; // Servidor SMTP
+            $mail->Host = 'mail.muninuevochimbote.gob.pe';
             $mail->SMTPAuth = true;
-            $mail->Username = 'alumbradopublico@muninuevochimbote.gob.pe'; // Correo de autenticación
-            $mail->Password = 'MDNCH*2025'; // Contraseña
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Usar SSL
-            $mail->Port = 465; // Puerto SMTP con SSL
+            $mail->Username = 'alumbradopublico@muninuevochimbote.gob.pe';
+            $mail->Password = 'MDNCH*2025';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
 
-            // Configuración del correo
-            $mail->CharSet = 'UTF-8'; // Establecer la codificación de caracteres a UTF-8
-            $mail->Encoding = 'base64'; // Establecer la codificación del contenido a base64
+            $mail->CharSet = 'UTF-8';
+            $mail->Encoding = 'base64';
             $mail->setFrom('alumbradopublico@muninuevochimbote.gob.pe', 'Problemas de alumbrado público');
-            $mail->addAddress($to); // Correo destinatario
-            $mail->Subject = $subject; // Asunto del correo
-            $mail->Body = $message; // Cuerpo del mensaje
-            $mail->isHTML(true); // Establecer el formato del correo a HTML
+            $mail->addAddress($to);
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+            $mail->isHTML(true);
 
-            // Adjuntar las imágenes si existen
             if (!empty($attachments)) {
                 foreach ($attachments as $attachment) {
                     $mail->addAttachment($attachment);
                 }
             }
 
-            // Enviar el correo
             $mail->send();
             return true;
         } catch (Exception $e) {
