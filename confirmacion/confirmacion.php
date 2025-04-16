9ac59666a1e96fe16a4549784e999bb8869f0ff5
Isaac-UTP <u2120529@utp.edu.pe>
2025-04-16 12:29:36 -0500

    Remove deprecated files and chatbot functionality from the public space reporting system, including HTML, JavaScript, and PHP scripts. Clean up unused assets and ensure no residual code remains.

--- confirmacion/confirmacion.php
+++ confirmacion/confirmacion.php
@@ -12,25 +12,10 @@
 
     <div class="container">
         <div class="image-container">
-            <img src="imagen1.jpg" alt="Imagen 1" class="active">
-            <img src="imagen2.jpg" alt="Imagen 2">
-            <img src="imagen3.jpg" alt="Imagen 3">
-            <img src="imagen4.jpg" alt="Imagen 4">
-        </div>
-        <div class="pillars-container">
-            <div class="pillar pillar-small"></div>
-            <div class="pillar pillar-medium"></div>
-            <div class="pillar pillar-center"></div>
-            <div class="pillar pillar-medium"></div>
-            <div class="pillar pillar-small"></div>
-            <div class="message-box">
-                ¡GRACIAS POR COMPARTIR TU OPINIÓN NUEVO CHIMBOTANO; CON TÚS REPORTES NOS AYUDAS A TOMAR ACCIÓN Y A
-                CONSTRUIR UN FUTURO MEJOR PARA LOS CIUDADANOS!
-            </div>
+            <img src="peru.png" alt="Imagen 1" class="active">
         </div>
     </div>
 
-    <script src="../js/confirmacion.js"></script>
 </body>
 
 </html>
